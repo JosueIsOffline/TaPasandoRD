18512406847c5d06164b418a0066d0f8010600db
JosueIsOffline <josuehernandez2314@gmail.com>
2025-08-20 00:53:53 -0400

    feat: add loadComment and create comment functionalities

--- public/assets/map/mapFeature.js
+++ public/assets/map/mapFeature.js
@@ -56,71 +56,89 @@ document.addEventListener("DOMContentLoaded", function () {
       }),
     };
 
-    function abrirModal(incident) {
-        document.getElementById("incidentTitle").textContent =
-            incident.title || "";
+    async function abrirModal(incident) {
+      document.getElementById("incidentTitle").textContent =
+        incident.title || "";
 
-        document.getElementById("incidentType").textContent =
-            getCategoryName(incident.category_id) || "Sin categoría";
+      document.getElementById("incidentType").textContent =
+        getCategoryName(incident.category_id) || "Sin categoría";
 
-        document.getElementById("incidentStatus").textContent =
-            incident.status || "Pendiente";
+      document.getElementById("incidentStatus").textContent =
+        incident.status || "Pendiente";
 
-        document.getElementById("locationInfo").innerHTML =
-            `<i class="fas fa-map-marker-alt me-1"></i> Provincia ID: ${incident.province_id}, Municipio ID: ${incident.municipality_id}`;
+      document.getElementById("locationInfo").innerHTML =
+        `<i class="fas fa-map-marker-alt me-1"></i> Provincia ID: ${incident.province_id}, Municipio ID: ${incident.municipality_id}`;
 
-        document.getElementById("timeInfo").innerHTML =
-            `<i class="fas fa-clock me-1"></i> ${incident.occurrence_date || ""}`;
+      document.getElementById("timeInfo").innerHTML =
+        `<i class="fas fa-clock me-1"></i> ${incident.occurrence_date || ""}`;
 
-        document.getElementById("incidentDescription").textContent =
-            incident.description || "";
+      document.getElementById("incidentDescription").textContent =
+        incident.description || "";
 
-        document.getElementById("deathCount").textContent =
-            incident.deaths ?? 0;
+      document.getElementById("deathCount").textContent = incident.deaths ?? 0;
 
-        document.getElementById("injuredCount").textContent =
-            incident.injuries ?? 0;
+      document.getElementById("injuredCount").textContent =
+        incident.injuries ?? 0;
 
-        document.getElementById("lossAmount").textContent =
-            `RD$${incident.estimated_loss || "0"}`;
+      document.getElementById("lossAmount").textContent =
+        `RD$${incident.estimated_loss || "0"}`;
 
-        document.getElementById("coordinatesDisplay").textContent =
-            `Latitud: ${incident.latitude} | Longitud: ${incident.longitude}`;
+      document.getElementById("coordinatesDisplay").textContent =
+        `Latitud: ${incident.latitude} | Longitud: ${incident.longitude}`;
 
-        document.getElementById("reportedBy").textContent =
-            `Usuario ${incident.reported_by || "Desconocido"}`;
+      document.getElementById("reportedBy").textContent =
+        `Usuario ${incident.reported_by || "Desconocido"}`;
 
-        document.getElementById("reportDate").textContent =
-            incident.created_at || "-";
+      document.getElementById("reportDate").textContent =
+        incident.created_at || "-";
 
-        document.getElementById("reportStatus").textContent =
-            incident.status || "Pendiente";
+      document.getElementById("reportStatus").textContent =
+        incident.status || "Pendiente";
 
-        // Imagen
-        if (incident.photo_url) {
-            document.getElementById("incidentPhoto").src = incident.photo_url;
-        }
-
-       if (incident.social_media_url) {
-
-          const contenedor = document.getElementById("referencias-container");
-          contenedor.innerHTML = "";
-
-          const boton = document.createElement("a");
-          boton.href = incident.social_media_url;
-          boton.target = "_blank";
-          boton.className = "btn btn-outline-primary btn-sm me-2";
-          boton.textContent = "Ver publicación";
-
-          contenedor.appendChild(boton);
+      // Imagen
+      if (incident.photo_url) {
+        document.getElementById("incidentPhoto").src = incident.photo_url;
       }
 
+      if (incident.social_media_url) {
+        const contenedor = document.getElementById("referencias-container");
+        contenedor.innerHTML = "";
 
-        new bootstrap.Modal(document.getElementById("incidentModal")).show();
-        const modal = bootstrap.Modal.getInstance(document.getElementById('incidentModal'));
-        modal.hide();
-}
+        const boton = document.createElement("a");
+        boton.href = incident.social_media_url;
+        boton.target = "_blank";
+        boton.className = "btn btn-outline-primary btn-sm me-2";
+        boton.textContent = "Ver publicación";
 
+        contenedor.appendChild(boton);
+      }
+
+      await loadComments(incident.id);
+      document
+        .getElementById("postComment")
+        .addEventListener("click", async () => {
+          const content = document.getElementById("commentText");
+          const data = new FormData();
+
+          data.append("incident_id", incident.id);
+          data.append("content", content.value);
+
+          console.log(data.get("content"));
+          const res = await fetch("/api/comment", {
+            method: "POST",
+            body: data,
+          });
+
+          await loadComments(incident.id);
+          content.value = "";
+        });
+
+      new bootstrap.Modal(document.getElementById("incidentModal")).show();
+      const modal = bootstrap.Modal.getInstance(
+        document.getElementById("incidentModal"),
+      );
+      modal.hide();
+    }
 
     const getCategoryName = (categoryId) => {
       const names = {
@@ -133,7 +151,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return names[categoryId];
     };
 
-  // Función para obtener provincias y categorías desde el backend por una API
+    // Función para obtener provincias y categorías desde el backend por una API
     // // Cargar las provincias y tipos de incidencia
     // async function loadFilters() {
     //   try {
@@ -169,13 +187,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // await loadFilters();
 
     async function loadIncidencias() {
-
       try {
         const provinciaId = document.getElementById("filterProvincia").value;
         const tipoId = document.getElementById("filterTipo").value;
         const fechaInicio = document.getElementById("filterFechaInicio").value;
         const fechaFin = document.getElementById("filterFechaFin").value;
-
 
         let url = new URL("http://localhost:8000/api/valid-incident");
         const response = await fetch(url);
@@ -235,3 +251,41 @@ document.addEventListener("DOMContentLoaded", function () {
   })();
 });
 
+function timeAgo(dateString) {
+  const date = new Date(dateString);
+  const now = new Date();
+  const diff = (now - date) / 1000;
+
+  if (diff < 60) return `Hace ${Math.floor(diff)} segundos`;
+  if (diff < 3600) return `Hace ${Math.floor(diff / 60)} minutos`;
+  if (diff < 86400) return `Hace ${Math.floor(diff / 3600)} horas`;
+  if (diff < 2592000) return `Hace ${Math.floor(diff / 86400)} días`;
+
+  return date.toLocaleDateString();
+}
+
+async function loadComments(id) {
+  const commentsCont = document.getElementById("comments-container");
+
+  try {
+    const res = await fetch(`/api/comment/${id}`);
+    const comments = await res.json();
+
+    commentsCont.innerHTML = comments
+      .map(
+        (comment) =>
+          `
+      <div class="comment mb-3 p-3 bg-light rounded">
+          <div class="d-flex justify-content-between align-items-start mb-2">
+             <strong>${comment.user_name}</strong>
+             <small class="text-muted">${timeAgo(comment.created_at)}</small>
+          </div>
+          <p class="mb-0">${comment.content}</p>
+      </div>
+`,
+      )
+      .join("");
+  } catch (error) {
+    console.error("Something went wrong loading comments", error);
+  }
+}
